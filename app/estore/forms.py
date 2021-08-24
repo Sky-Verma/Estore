a16a30745fe336c86bd1380b7aab5c2d1cc3af5d
Sky Verma <52072723+Sky-Verma@users.noreply.github.com>
2021-08-24 11:25:03 +0530

    Delete app directory

--- app/estore/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import user_info
-
-class user_info_form(ModelForm):
-
-    class Meta:
-        model = user_info
-        fields = ['profile_image','phone_number','about']
-
--- app/estore/static/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.profile_pic_p').innerHTML = '<input type="file" name="profile_pic" accept="image/*" id="id_profile_pic">';
-var textarea = document.querySelector('textarea');
-textarea.rows = 2;
-textarea.cols = 20;
-
-var code = document.querySelector('.code');
-
-code.textContent = window.location.host + '/code/'+code.textContent;
